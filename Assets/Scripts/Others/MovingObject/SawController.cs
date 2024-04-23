6d804d63c0f1b8a5e58ef75ba7a776ed0f04cc2e
1609Dzuaa <106080372+1609Dzuaa@users.noreply.github.com>
2024-04-23 12:18:07 +0700

    init moving obj

---
+++ Assets/Scripts/Others/MovingObject/SawController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class SawController : MovingObjectController
+{
+    [SerializeField] float _rotateSpeed;
+    [SerializeField] bool _isBossGate;
+
+    protected override void Start()
+    {
+        if (_isBossGate)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    private void OnDestroy()
+    {
+        if (_isBossGate)
+        {
+            //Debug.Log("Unsub");
+        }
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        if (!_isBossGate)
+            base.Update();
+        transform.Rotate(0f, 0f, 360f * _rotateSpeed * Time.deltaTime);
+    }
+
+    /*private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(GameConstants.SHIELD_TAG))
+            gameObject.SetActive(false);
+    }*/
+
+    /*/// <summary>
+    /// Hàm dưới để active gate khi Player enter Boss Room
+    /// </summary>*/
+
+    /*private void ActiveGate(object obj)
+    {
+        Debug.Log("activated");
+        GameObject activeVfx = Pool.Instance.GetObjectInPool(GameEnums.EPoolable.RedExplode);
+        activeVfx.SetActive(true);
+        activeVfx.transform.position = transform.position;
+        gameObject.SetActive(true);
+    }*/
+}
